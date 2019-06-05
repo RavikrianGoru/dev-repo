7faabe71b98c163faabadd9a4caba1bec49a3865
ravikiran.goru <ravikirangoru@gmail.com>
2019-06-06 00:13:33 +0530

    Update jackson version and Date Ranges overlaps

--- proj/corejava/src/main/java/interviews/asg/MovieNight.java
+++ proj/corejava/src/main/java/interviews/asg/MovieNight.java
@@ -1,23 +1,30 @@
 package interviews.asg;
 
-import java.util.*;
 import java.text.SimpleDateFormat;
-import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
 
 public class MovieNight {
 
 	  public static Boolean canViewAll(Collection<Movie> movies) {
 			boolean flag = true;
 			ArrayList<Movie> movieList = (ArrayList<Movie>) movies;
-			Comparator<Movie> movieComparator = (o1, o2) -> ((int) (o1.getStart().getTime() - o2.getStart().getTime()));
-			Collections.sort(movieList, movieComparator);
-			
+//			Comparator<Movie> movieComparator = (o1, o2) -> ((int) (o1.getStart().getTime() - o2.getStart().getTime()));
+//			Collections.sort(movieList, movieComparator);
+			Collections.sort(movieList, (o1, o2) -> ((int) (o1.getStart().getTime() - o2.getStart().getTime())));
 			for (int index = 0; index < movieList.size() - 1; index++) {
 				long startDate1 = movieList.get(index).getStart().getTime();
 				long endDate1 = movieList.get(index).getEnd().getTime();
 				long startDate2 = movieList.get(index + 1).getStart().getTime();
 				long endDate2 = movieList.get(index + 1).getEnd().getTime();
 				flag = startDate1 <= endDate1 && endDate1<= startDate2 && startDate2 <= endDate2;
+				if(!flag)
+				{
+					return flag;
+				}
 			}
 			return flag;
 		}
@@ -26,7 +33,7 @@ public class MovieNight {
 		SimpleDateFormat sdf = new SimpleDateFormat("y-M-d H:m");
 
 		ArrayList<Movie> movies = new ArrayList<Movie>();
-		movies.add(new Movie(sdf.parse("2015-01-01 21:30"), sdf.parse("2015-01-01 23:00")));
+//		movies.add(new Movie(sdf.parse("2015-01-01 21:30"), sdf.parse("2015-01-01 23:00")));
 		movies.add(new Movie(sdf.parse("2015-01-01 20:00"), sdf.parse("2015-01-01 21:30")));
 		movies.add(new Movie(sdf.parse("2015-01-01 23:10"), sdf.parse("2015-01-01 23:30")));
 		movies.add(new Movie(sdf.parse("2015-01-01 21:30"), sdf.parse("2015-01-01 23:00")));
